421f1bdb1adcdfec6fc8f92c8c67882657ffa1f9
NewLife2022 <1113010571@qq.com>
2023-07-16 15:00:48 +0800

    全量上传

---
+++ src/main/java/com/springboot/JavaEEBackApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEEBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEEBackApplication.class, args);
+    }
+
+}
